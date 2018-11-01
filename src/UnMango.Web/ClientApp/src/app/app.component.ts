8bb5029a65a3497d13498883be0690f52c5e32ee
Erik Rasmussen <erik.rasmussen@kumandgo.com>
2018-11-01 13:47:24 -0500

    Updating nav-menu and adding material

--- src/UnMango.Web/ClientApp/src/app/app.component.ts
+++ src/UnMango.Web/ClientApp/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { MatSidenavContainer } from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
   title = 'app';
+
+  @ViewChild(MatSidenavContainer)
+  sidenavContainer: MatSidenavContainer;
+
+  ngAfterViewInit() {
+    this.sidenavContainer.scrollable.elementScrolled().subscribe(() => /* react to scrolling */{});
+  }
+
 }
--- src/UnMango.Web/ClientApp/src/app/components/index.ts
+++ src/UnMango.Web/ClientApp/src/app/components/index.ts
@@ -2,15 +2,18 @@ import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { HomeComponent } from './home/home.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { TitlebarComponent } from './titlebar/titlebar.component';
 
 export const components = [
   CounterComponent,
   FetchDataComponent,
   HomeComponent,
-  NavMenuComponent
+  NavMenuComponent,
+  TitlebarComponent
 ];
 
 export { CounterComponent } from './counter/counter.component';
 export { FetchDataComponent } from './fetch-data/fetch-data.component';
 export { HomeComponent } from './home/home.component';
 export { NavMenuComponent } from './nav-menu/nav-menu.component';
+export { TitlebarComponent } from './titlebar/titlebar.component';
